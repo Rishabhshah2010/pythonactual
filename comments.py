e3a27330a082f67416236876550d9c1b9f9de0dd
Rishabhshah2010 <98174937+Rishabhshah2010@users.noreply.github.com>
2024-02-04 17:19:04 +0530

    python

---
+++ comments.py
@@ -0,0 +1,9 @@
+# use hash for single line comment
+# use it to explain code
+x = 9 # x is a variable that stores nine
+# a= 10
+# print(a)
+
+
+
+# there is no special syntax for constants
---
+++ varlearnin.py
@@ -0,0 +1,20 @@
+miles = 10
+miles = 15
+a,b,c =1,2,3
+print(a)
+print(b)
+print(c)
+myname = 'Rishabh'
+
+
+
+#naming convention
+
+my_name = 'Rishabh' # snake case
+age = 13
+day_1 = 'Today'
+
+_bonus =100 # avoid
+# also avoid starting with number and no special characters
+# use no important words
+
