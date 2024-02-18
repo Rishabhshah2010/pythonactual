ea6e277eb805f0fe63ab5b2ce54261c2aabf9dae
Rishabhshah2010 <98174937+Rishabhshah2010@users.noreply.github.com>
2024-02-18 19:02:39 +0530

    started strings
    was distracted by studies

---
+++ strings.py
@@ -0,0 +1,16 @@
+my_str1 = 'I am Rishabh'
+
+print('i\'m so cool')
+print("i'm so cool")
+
+print(type(my_str1))
+
+languages = """  I like Python
+Java
+
+"""
+
+print(languages)
+
+lol = "I like Python\nand\nJava"
+print(lol)
---
+++ user_input.py
@@ -0,0 +1,7 @@
+#name = input("Enter your name:")
+#print(name)
+
+price =input("Enter price ")
+quantity = input("Enter quantity ")
+total_price = float(price) * float(quantity)
+print(total_price)
